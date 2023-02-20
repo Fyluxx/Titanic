f000567bcedf65c9cec866af1702d32ed5b2e133
Fynn Luhr <f.luhr@paraboost.de>
2023-02-20 14:29:20 +0100

    Read csv File

---
+++ ReadFiles.py
@@ -0,0 +1,10 @@
+import csv
+
+with open("./Daten/train.csv", "r") as file:
+    global csvTrain
+    csvTrain = [tuple(row) for row in list(csv.reader(file))]
+
+
+with open("./Daten/test.csv", "r") as file:
+    global csvTest
+    csvTest = [tuple(row) for row in csv.reader(file)]
---
+++ TransformData.py
@@ -0,0 +1,3 @@
+import ReadFiles as rf
+
+testingData, 
