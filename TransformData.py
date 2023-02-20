1820b06a67f595f52874aaeeb904439806fb8b3f
Fynn Luhr <f.luhr@paraboost.de>
2023-02-20 15:56:09 +0100

    improved code

--- TransformData.py
+++ TransformData.py
@@ -5,19 +5,21 @@ import ReadFiles as rf
 #from sk.model_selection import train_test_split
 
 survivedCell = 1
-x, y = [()], []
+x, y = [], []
 
 arrLengthFirst = len(rf.csvTrain)
-for i in range(1, arrLengthFirst):
+for i in range(arrLengthFirst):
+    if i == 0:
+        continue
     arrLengthSecond = len(rf.csvTrain[i])
-    x.append(())
-    for j in range (arrLengthSecond):
+    tup = ()
+    for j in range(arrLengthSecond):
         if j == survivedCell:
             y.append(rf.csvTrain[i][j])
         else:
-            tup =  (rf.csvTrain[i][j],)
-            k = 889
-            x[i] = x[i] + tup
+            tup += (rf.csvTrain[i][j],)
+    x.append(tup)
+
 print(len(x))
 
 print(x)
