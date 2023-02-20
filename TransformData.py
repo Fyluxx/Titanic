f3c740da78c5d0ea78e92c69d97c188b5405d190
Fynn Luhr <f.luhr@paraboost.de>
2023-02-20 15:54:51 +0100

    split data into x and y

--- TransformData.py
+++ TransformData.py
@@ -1,7 +1,26 @@
-import numpy as np
-import sklearn as skip
+#import numpy as np
+#import sklearn as sk
 import ReadFiles as rf
 
-testingData, validationData = [], []
-print(rf.csvTest)
-a = np.arange(15).reshape(3,5)
+#from sk.model_selection import train_test_split
+
+survivedCell = 1
+x, y = [()], []
+
+arrLengthFirst = len(rf.csvTrain)
+for i in range(1, arrLengthFirst):
+    arrLengthSecond = len(rf.csvTrain[i])
+    x.append(())
+    for j in range (arrLengthSecond):
+        if j == survivedCell:
+            y.append(rf.csvTrain[i][j])
+        else:
+            tup =  (rf.csvTrain[i][j],)
+            k = 889
+            x[i] = x[i] + tup
+print(len(x))
+
+print(x)
+    
+    
+    
