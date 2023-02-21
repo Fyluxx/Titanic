c2338edd01e6c91f87fa56d9a584d6373e7325b1
Fynn Luhr <f.luhr@paraboost.de>
2023-02-21 10:00:43 +0100

    Added timer for debugging

--- ReadFiles.py
+++ ReadFiles.py
@@ -1,4 +1,7 @@
 import csv
+import time
+
+beginn = time.time()
 
 with open("./Daten/train.csv", "r") as file:
     global csvTrain
@@ -8,3 +11,7 @@ with open("./Daten/train.csv", "r") as file:
 with open("./Daten/test.csv", "r") as file:
     global csvTest
     csvTest = [tuple(row) for row in csv.reader(file)]
+
+end = time.time()
+
+print(str(end-beginn) + "So lange hat das gedauert" + "\n\n\n")
